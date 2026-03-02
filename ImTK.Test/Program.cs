393b98234811afb88cf7b4bd6a47f217c82d7fed
ysnaive <yi.shi.4589@gmail.com>
2026-03-02 22:07:31 +0800

    env setup

---
+++ ImTK.Test/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ImTK.Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, ImGui.NET!");
+        }
+    }
+}
